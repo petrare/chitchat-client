9923554a0232e073cc490649b1c6bf6aa774b7f7
Petra Reberc <petrareberc@hotmail.com>
2017-06-07 09:40:20 +0200

    prvi commit ki hopefully dela?

--- src/main/java/si/reberc/chitchat/App.java
+++ src/main/java/si/reberc/chitchat/App.java
@@ -1,8 +1,12 @@
-package my.group.chitchat;
+package si.reberc.chitchat;
 
+import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.fluent.Request;
+import org.apache.http.client.utils.URIBuilder;
 
 import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 /**
  * Hello ChitChat!
@@ -14,8 +18,62 @@ public class App {
                                   .execute()
                                   .returnContent().asString();
             System.out.println(hello);
+            
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
+    
+    public static void seznamUporabnikov() {
+        try {
+            String uporabniki = Request.Get("http://chitchat.andrej.com/users")
+                                  .execute()
+                                  .returnContent().asString();
+            System.out.println(uporabniki);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+   
+
+
+
+    public static void prijava() {
+        try {
+            URI uri = new URIBuilder("http://chitchat.andrej.com/users").addParameter("username", "Petra").build();
+            String responseBody = Request.Post(uri).execute().returnContent().asString();
+            System.out.println(responseBody);
+
+        } catch (URISyntaxException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (ClientProtocolException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+    }
+    
+    public static void odjava() {
+        try {
+            URI uri = new URIBuilder("http://chitchat.andrej.com/users").addParameter("username", "Petra").build();
+            String responseBody = Request.Delete(uri).execute().returnContent().asString();
+            System.out.println(responseBody);
+
+        } catch (URISyntaxException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (ClientProtocolException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
 }
+    
+
